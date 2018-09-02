dc745fbcc8f7fab8122a617f608d7cd0c98f42c3
Toyz <Twozy@163.com>
2018-09-02 11:10:49 +0800

    用户信息右上角的信息展示前后端实现 注意打开用户登录成功后的display:block;

--- info/module/index/views.py
+++ info/module/index/views.py
@@ -1,13 +1,20 @@
 from info.module.index import index_bp
-from flask import render_template, current_app
-from info import models
+from flask import render_template, current_app, g
+from info.utlis.common import login_user_data
 
 
 @index_bp.route("/")
+@login_user_data
 def index():
 
+    user = g.user
+
+    data = {
+        "user_info": user.to_dict() if user else None
+    }
+    # print(data)
     print(current_app.url_map)
-    return render_template("index.html")
+    return render_template("index.html", data=data)
 
 
 @index_bp.route("/favicon.ico")
