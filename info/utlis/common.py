4b886b4fb0188265502c9ebf19eb66342ac1f5ab
Toyz <Twozy@163.com>
2018-09-02 10:52:33 +0800

    纠正装饰器的封装 为了后续{{ url_for("蓝图名.函数名") }} 可以获取到准确的url, 所以需要避免装饰视图函数后, 视图函数名都变成了wrapper, 所以需要增加一个特殊的装饰器

--- info/utlis/common.py
+++ info/utlis/common.py
@@ -1,3 +1,4 @@
+import functools
 from flask import current_app
 from flask import g
 from flask import session
@@ -5,18 +6,20 @@ from flask import session
 
 
 def login_user_data(view_func):
+    @functools.wraps(view_func)
     def wrapper(*args, **kwargs):
-        user_id = session["user_id"]
+        user_id = session.get("user_id")
         user = []
 
+        # 为了避免循环导入, 哪里用哪里导
         from info.models import User
         if user_id:
             try:
-                user = User.query.filter(User.id == user_id).first()
+                user = User.query.get(user_id)
             except Exception as e:
                 current_app.logger.error(e)
 
         g.user = user
-
-        return view_func(*args, **kwargs)
+        result = view_func(*args, **kwargs)
+        return result
     return wrapper
